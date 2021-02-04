254435f149fab837ea4cbd4bd0cffc4b6bbdda4f
buildgal <gjaquelin7@outlook.com>
2021-02-03 23:17:18 -0600

    updated tech projects page

---
+++ portfilio.js
@@ -0,0 +1,10 @@
+
+let login = document.getElementById("loginImg");
+
+function displayDashboard(x) {
+    login.src= "./Images/dashboardP2.JPG";
+  }
+
+  function displayLogin(x) {
+    login.src= "./Images/loginP2.JPG";
+  }
