1e594a7bd9aad4d66eff9f46296c873d0c6924f2
buildgal <gjaquelin7@outlook.com>
2021-03-11 00:12:15 -0600

    add the psuedo code for REACT

--- portfilio.js
+++ portfilio.js
@@ -8,3 +8,22 @@ function displayDashboard(x) {
   function displayLogin(x) {
     login.src= "./Images/loginP2.JPG";
   }
+
+  //react psuedo code 
+
+  //import header from "path",
+  //import project from "path",
+  //import footer from "path",
+
+  //<Header>
+  //Add the funcationality to route each page 
+  //</Header>
+
+   //<Footer>
+  //this should be good to go 
+  //</Footer>
+
+   //<Project>
+   //this one will need to display the data for each project 
+   //add the URL, descriptions, and Images to a JSON to be imported by cards 
+  //</Proje>ct 
